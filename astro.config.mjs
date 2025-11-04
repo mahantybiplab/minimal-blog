2f561909ee8e06ae34e38547599c4823f64c5cf4
biplab mahanty <bmahanty501@gmail.com>
2025-11-04 17:24:24 +0530

    feat: add ArticleCard and ArticleList components for blog post display
    - Created ArticleCard component to display individual blog post details including title, subtitle, description, and a read more link.
    - Implemented ArticleList component to render a list of ArticleCard components by importing posts from the specified directory.
    - Added PostMeta component to show metadata for each post, including publication date and tags.
    - Introduced Prose component for consistent styling of blog content.
    - Added SVG icon for enhanced UI.
    - Created initial blog posts with metadata for testing the new components.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://biplabmahanty.netlify.app",
   vite: { plugins: [tailwindcss()] },
+  integrations: [icon(), mdx()],
 });
